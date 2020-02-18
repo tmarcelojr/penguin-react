9c8b62e9c8e0888cf13ba7d530de8efd874a207b
Teo Marcelo <tmarcelojr@gmail.com>
2020-02-18 11:22:27 -0600

    rendering penguin container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PenguinContainer from './PenguinContainer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Penguin React</h1>
+      <PenguinContainer />
     </div>
   );
 }
---
+++ src/PenguinContainer/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class PenguinContainer extends Component {
+	state = {
+
+	}
+
+	render(){
+		return(
+			<React.Fragment>
+				<h2>Hello this is our penguin container</h2>
+			</React.Fragment>
+			)
+	}
+}
